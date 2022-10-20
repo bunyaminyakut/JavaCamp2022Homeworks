ac26f21e059deafc63cc4ce6e89a82113c32c8ab
Bünyamin Yakut <80539363+bunyaminyakut@users.noreply.github.com>
2022-10-20 23:42:46 +0300

    JavaCamp

---
+++ 3.Hafta/Ödev1/youTubeDemo/src/youTubeDemo/CarCreditManager.java
@@ -0,0 +1,12 @@
+package youTubeDemo;
+
+public class CarCreditManager extends BaseCreditManager implements ICreditManager {
+
+	@Override
+	public void Calculate() {
+		
+		System.out.println("Araba Kredisi Verildi");
+		
+	}
+
+}
---
+++ 3.Hafta/Ödev1/youTubeDemo/src/youTubeDemo/Customer.java
@@ -0,0 +1,13 @@
+package youTubeDemo;
+
+public class Customer {
+	
+	public Customer()
+	{
+		System.out.println("Müşteri Nesnesi Başlatıldı");
+	}
+	
+	public int id;
+	public String city;
+	
+}
